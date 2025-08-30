0cbd0ffa6430cdbd3f3a4a0704859fffd23dc1f3
Ivan Mikushin <imikushin@users.noreply.github.com>
2025-08-30 10:55:14 -0700

    Update bro-token scripts and spells to version 7
    - Bump spell versions to 7 in YAML files.
    - Refactor export scripts to include `app_bins` variable initialization and update UTXO references.

--- bro-token/script/export-vars-nft.sh
+++ bro-token/script/export-vars-nft.sh
@@ -1,12 +1,12 @@
+app_bins=$(charms app build)
 
-export app_vk=$(charms app vk)
-export in_utxo_0="4988e8a6ad5eb660d1135075a2674135855b40ddbbabbe6dfa7a7207c241c278:1"
+export app_vk=$(charms app vk ${app_bins})
+export in_utxo_0="344c5dcb694f18fd0c31e5d1897efe2d33ffafcd2f1904e0f78cb7c26f9d885b:1"
 export app_id=$(echo -n "${in_utxo_0}" | sha256sum | cut -d' ' -f1)
 
 export addr_0=$(b getnewaddress)
 
 prev_txs=$(b getrawtransaction ${in_utxo_0%%:*})
-app_bins=$(charms app build)
 
 funding_utxo=4e97509ac6d3cc496c229a3433dd3bd2b495034a95a5eb21027d1a80a1b3829e:2
 funding_utxo_value=$(b gettxout ${funding_utxo%%:*} ${funding_utxo##*:} | jq '(.value * 100000000) | floor')
--- bro-token/script/export-vars-token.sh
+++ bro-token/script/export-vars-token.sh
@@ -1,5 +1,6 @@
+app_bins=$(charms app build)
 
-export app_vk=$(charms app vk)
+export app_vk=$(charms app vk ${app_bins})
 export in_utxo_0="4988e8a6ad5eb660d1135075a2674135855b40ddbbabbe6dfa7a7207c241c278:1"
 export app_id=$(echo -n "${in_utxo_0}" | sha256sum | cut -d' ' -f1)
 export mining_txid=732337e4a15ef24b2e49f83ab397e305e60563615fc9710aa5616e68e2e66774
@@ -13,8 +14,7 @@ export addr_1=$(b getnewaddress)
 export mined_amount=36100000000
 
 prev_txs=$(b getrawtransaction ${mining_txid})
-app_bins=$(charms app build)
 
-funding_utxo=118aca1eadce14ef507eff39651e680f55b694b82ae27ac8b3722972976306a6:2
+funding_utxo=c29691b6cd86132a01708ae01d466b5a16f4d623fcd23a25aa658501817fb3e1:1
 funding_utxo_value=$(b gettxout ${funding_utxo%%:*} ${funding_utxo##*:} | jq '(.value * 100000000) | floor')
 change_address=$(b getrawchangeaddress)
